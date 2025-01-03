b457856a485ba48bcdbbdab921d8ab9717269f99
BigyanKhanal33 <knlbgyan5523@gmail.com>
2025-01-03 15:34:13 +0545

    Updated utils package and added main script

---
+++ utils/sys_practice.py
@@ -0,0 +1,13 @@
+import sys
+
+# Get the version of Python
+python_version = sys.version
+print(f"Python Version: {python_version}")
+
+# Get the list of command-line arguments
+command_line_args = sys.argv
+print(f"Command Line Arguments: {command_line_args}")
+
+# Exit from Python
+print("Exiting...")
+sys.exit()
